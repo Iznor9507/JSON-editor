9c06d4856d092a6a996a404c5a4e75662c049894
iznor <iznor.hucuev9507@gmail.comm>
2023-06-20 01:30:34 +0300

    добавил комментарии к коду

--- src/components/JsonEditor.tsx
+++ src/components/JsonEditor.tsx
@@ -1,14 +1,19 @@
 import { memo, useEffect, useState } from "react";
 import { Input } from "antd";
 import { JsonService } from "../services/JsonSirvice";
-
+import style from "./JsonEditor.module.css";
 const { TextArea } = Input;
 
 const JsonEditor = memo(() => {
+  // для хранения текста json
   const [jsonText, setJsonText] = useState("");
+
+  //для хранения состояния ощибки
   const [error, setError] = useState(false);
 
   useEffect(() => {
+    // сохраняем данныее из localStorage под ключом jsonText,
+    // и если есть, то мы их извлекаем и устанавливаем в setJsonText
     const storedJsonText = localStorage.getItem("jsonText");
     if (storedJsonText) {
       setJsonText(storedJsonText);
@@ -16,6 +21,10 @@ const JsonEditor = memo(() => {
   }, []);
 
   useEffect(() => {
+    // вызывается каждый раз при изменении jsonText
+    //  Он сохраняет новый текст в localStorage,
+    //   а затем пытается его разобрать с помощью `JsonService.parseJson()`.
+    //   Если разбор проходит успешно, `error` устанавливается в `false иначе в `true`
     localStorage.setItem("jsonText", jsonText);
     try {
       const json = JsonService.parseJson(jsonText);
@@ -28,33 +37,16 @@ const JsonEditor = memo(() => {
 
   return (
     <div>
+      {/* поле ввода */}
       <TextArea
         value={jsonText}
         onChange={(e) => setJsonText(e.target.value)}
         rows={10}
       />
       {error ? (
-        <h1
-          style={{
-            backgroundColor: "red",
-            width: "16%",
-            color: "white",
-            borderRadius: "5px",
-          }}
-        >
-          Это не JSON код!
-        </h1>
+        <h1 className={style.not_json}>Это не JSON код!</h1>
       ) : (
-        <h1
-          style={{
-            backgroundColor: "#34C924",
-            width: "24%",
-            color: "white",
-            borderRadius: "5px",
-          }}
-        >
-          Это валидный JSON код!
-        </h1>
+        <h1 className={style.no_error}>Это валидный JSON код!</h1>
       )}
     </div>
   );
--- src/services/JsonService.test.ts
+++ src/services/JsonService.test.ts
@@ -1,5 +1,8 @@
 import { JsonService } from "./JsonSirvice";
 
+
+
+
 test("parseJson", () => {
   expect(JsonService.parseJson('{"key":"value"}')).toEqual({ key: "value" });
   expect(() => JsonService.parseJson("invalid")).toThrow("Неправильный JSON");
