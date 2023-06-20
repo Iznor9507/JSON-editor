caf049ec401b8c04c86e64373e5c5b2c802cd4c1
iznor <iznor.hucuev9507@gmail.comm>
2023-06-20 12:03:21 +0300

    Отрефакторил JsonEditor

--- src/components/JsonEditor.tsx
+++ src/components/JsonEditor.tsx
@@ -1,17 +1,13 @@
 import { memo, useEffect, useState } from "react";
 import { Alert, Input } from "antd";
 import { JsonService } from "../services/JsonSirvice";
-import style from "./JsonEditor.module.css";
 const { TextArea } = Input;
 
 const JsonEditor = () => {
-
   const [jsonText, setJsonText] = useState("");
   const [error, setError] = useState(false);
 
   useEffect(() => {
-    // сохраняем данныее из localStorage под ключом jsonText,
-    // и если есть, то мы их извлекаем и устанавливаем в setJsonText
     const storedJsonText = localStorage.getItem("jsonText");
     if (storedJsonText) {
       setJsonText(storedJsonText);
@@ -19,10 +15,6 @@ const JsonEditor = () => {
   }, []);
 
   useEffect(() => {
-    // вызывается каждый раз при изменении jsonText
-    //  Он сохраняет новый текст в localStorage,
-    //   а затем пытается его разобрать с помощью `JsonService.parseJson()`.
-    //   Если разбор проходит успешно, `error` устанавливается в `false иначе в `true`
     localStorage.setItem("jsonText", jsonText);
     try {
       const json = JsonService.parseJson(jsonText);
@@ -41,19 +33,19 @@ const JsonEditor = () => {
         rows={10}
       />
       {error ? (
-         <Alert
-         message="Error"
-         description="Это не JSON"
-         type="error"
-         showIcon
-       />
+        <Alert
+          message="Ощибка"
+          description="Это не JSON"
+          type="error"
+          showIcon
+        />
       ) : (
         <Alert
-        message="Отлично"
-        description="Это валидный JSON"
-        type="success"
-        showIcon
-      />
+          message="Отлично"
+          description="Это валидный JSON"
+          type="success"
+          showIcon
+        />
       )}
     </>
   );
